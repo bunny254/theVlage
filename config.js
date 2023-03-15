7027bc3d07cb479ee83017138adf62196a532ec7
Harris <harriskarume.hk@gmail.com>
2023-03-15 23:17:32 +0300

    Source API url from .env

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import getConfig from 'next/config'
 const { publicRuntimeConfig } = getConfig()
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://backend.thevlage.com/api' : 'https://backend.thevlage.com/api'
+export const API = publicRuntimeConfig.PRODUCTION ? process.env.API_PRODUCTION_URL : process.env.API_LOCAL_URL
 export const APP_NAME = publicRuntimeConfig.APP_NAME
